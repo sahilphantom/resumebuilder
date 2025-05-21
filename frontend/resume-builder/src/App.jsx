07d117b6e39ee94ea2a588ae0f5780ee2fcd8f52
sahilphantom <sahilmahmood188@gmail.com>
2025-05-21 00:40:12 -0700

    restructure routing and implement LandingPage component

--- frontend/resume-builder/src/App.jsx
+++ frontend/resume-builder/src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import {browserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Toast} from 'react-hot-toast';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Dashboard from './pages/Home/Dashboard';
 
 const App = () => {
   return (
-    <div className='text-center text-gray-600  '>
-      <h1 className='text-3xl font-bold underline'>
-        Resume Builder
-      </h1>
-      App
-    </div>
+   <div>
+    <Router>
+      <Routes>
+        {/* default route */}
+        <Route path="/" element={<LandingPage/>} />
+        {/* auth routes */}
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+
+       <Route path="/dashboard" element={<Dashboard/>} />
+        {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
+        {/* <Route path="/resume/:id" element={<Resume/>} /> */}
+        <Route path="/resume/:resumeId" element={<EditResume/>} />
+        {/* <Route path="/resume/:id/preview" element={<PreviewResume/>} /> */}
+        {/* <Route path="/resume/:id/download" element={<DownloadResume/>} /> */}
+
+
+
+        </Routes>
+    </Router>
+   </div>
   )
 }
 
