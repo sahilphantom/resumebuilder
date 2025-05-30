8ada41eba8d9ee76aefbdc9974c569044490cd59
sahilphantom <sahilmahmood188@gmail.com>
2025-05-30 04:24:28 -0700

    fix: correct formatting in Resume model and ensure NODE_ENV is set in .env

--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ResumeSchema = new mongoose.Schema({    
   userId: { 
-	type: mongoose.Schema.Types.ObjectId, 
+	type: mongoose.Schema.Types.ObjectId , 
 	ref: "User", 
 	required: true
   }, 
